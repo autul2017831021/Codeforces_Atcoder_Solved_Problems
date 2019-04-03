753f9170a4bf302b9136a4579eb5c24485cd5768
autul2017831021 <alexiautul@gmail.com>
2019-04-03 23:19:00 +0600

    Add solution for problem `A - Game With Sticks` Link: https://codeforces.com/contest/451/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/451/submission/52281575

---
+++ codeforces/451/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+main ()
+{
+    int h,v;
+    cin>>h>>v;
+    if(h>v)
+    {
+        swap(h,v);
+    }
+    if(h%2==0)
+    {
+        cout<<"Malvika";
+    }
+    else
+
+    cout<<"Akshat";
+}
