1390f81aec2b58db7cfc5e1b63849c3f5f91dbd6
autul2017831021 <alexiautul@gmail.com>
2019-06-05 01:10:00 +0600

    Add solution for problem `A - The New Year: Meeting Friends` Link: https://codeforces.com/contest/723/problem/A Tags: implementation, math, sortings, *800 Ref: https://codeforces.com/contest/723/submission/55099421

---
+++ codeforces/723/A.cpp
@@ -0,0 +1,12 @@
+#include<bits//stdc++.h>
+using namespace std;
+main()
+{
+    int a[3];
+    for(int i=0;i<3;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+3);
+    cout<<((a[1]-a[0])+(a[2]-a[1]))<<endl;
+}
