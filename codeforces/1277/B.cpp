a91b0a2cc53640ccde500542289a87633f524f86
autul2017831021 <alexiautul@gmail.com>
2019-12-28 13:40:00 +0600

    Add solution for problem `B - Make Them Odd` Link: https://codeforces.com/contest/1277/problem/B Tags: greedy, number theory, *1200 Ref: https://codeforces.com/contest/1277/submission/67772968

---
+++ codeforces/1277/B.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,a,c=0,h,b;
+        cin>>n;
+        set<int,greater <int>>s;
+        vector<int>v;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a;
+            if(a%2==0)
+            {
+                s.insert(a);
+            }
+        }
+        while(1)
+        {
+            if(s.size()==0)
+                break;
+           b=*s.begin();
+           //cout<<"b:"<<b<<endl;
+           h=b/2;
+           if(h%2==0)
+            s.insert(h);
+           else
+           {
+           }
+           s.erase(b);
+           c++;
+
+        }
+        cout<<c<<endl;
+        c=0;
+    }
+}
+
