cb783d7f78ff326a4e606b13f726cbf72a7711fa
autul2017831021 <alexiautul@gmail.com>
2020-02-22 02:32:00 +0600

    Add solution for problem `A - Regular Bracket Sequence` Link: https://codeforces.com/contest/1132/problem/A Tags: greedy, implementation, *1100 Ref: https://codeforces.com/contest/1132/submission/71574587

---
+++ codeforces/1132/A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if(a==d && (c==0 || a>0) )
+        cout<<"1"<<endl;
+    else
+        cout<<"0"<<endl;
+}
