218d88de22b97907ffccd17fe53b5f2d6a88d5fd
autul2017831021 <alexiautul@gmail.com>
2020-03-02 02:03:00 +0600

    Add solution for problem `C - Balanced Team` Link: https://codeforces.com/contest/1133/problem/C Tags: sortings, two pointers, *1200 Ref: https://codeforces.com/contest/1133/submission/72215390

--- codeforces/1133/C.cpp
+++ codeforces/1133/C.cpp
@@ -1,56 +1,31 @@
-    #include<bits/stdc++.h>
-    using namespace std;
-    typedef long long ll;
-    long long int BS(ll a[],ll x,ll n)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+main()
+{
+    ll n,c=1,m=1,b=5,l=0,r=0,mid=0;
+    cin>>n;
+    vector<ll>v(n);
+    for(ll i=0;i<n;i++)
     {
-        ll l,r;
-        l=1;
-        r=n;
-            while(r>=l)
-            {
-                ll mid=(l+r)/2;
-                if(a[mid]>x)
-                    r=mid-1;
-                else
-                    l=mid+1;
-            }
-            return l;
+        cin>>v[i];
     }
-        #include<bits/stdc++.h>
-        using namespace std;
-        typedef long long ll;
-        main()
+    ll k=0,j;
+    sort(v.rbegin(),v.rend());
+    for(ll i=0;i<n;i++)
+    {
+        for(j=k;j<n;j++)
         {
-            ll n,c=1,m=1,b=5;
-            cin>>n;
-            vector<ll>v(n);
-            for(ll i=0;i<n;i++)
+            if(v[i]-v[j]<=5)
             {
-                cin>>v[i];
+                m=max(m,j-i+1);
             }
-            ;
-            sort(v.begin(),v.end());
-            for(ll i=0;i<n;i++)
+            else
             {
-                ll l=i;
-                ll r=n-1;
-                ll mid;
-                //cout<<i<<endl;
-                while(r>=l){
-                        //cout<<"I->"<<i<<" R->"<<r<<" L->"<<l<<endl;
-                        mid=(l+r)/2;
-                    if(v[mid]-v[i]<=5)
-                    {
-                        l=mid+1;
-                    }
-                    else
-                    {
-                        r=mid-1;
-                    }
-                }
-                //cout<<"bal->"<<(((r+l)/2))-i+1<<endl;
-                m=max(m,(((r+l)/2))-i+1);
-                //cout<<m<<endl;
+                break;
             }
-            cout<<m<<endl;
         }
+        k=j+1;
+    }
+    cout<<m<<endl;
+}
