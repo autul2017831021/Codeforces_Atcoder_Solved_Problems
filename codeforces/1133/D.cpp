1fc8d6f7973c2f33e276af7c804935ca99475296
autul2017831021 <alexiautul@gmail.com>
2020-03-03 05:04:00 +0600

    Add solution for problem `D - Zero Quantity Maximization` Link: https://codeforces.com/contest/1133/problem/D Tags: hashing, math, number theory, *1500 Ref: https://codeforces.com/contest/1133/submission/72277419

--- codeforces/1133/D.cpp
+++ codeforces/1133/D.cpp
@@ -1,53 +1,60 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef double ll;
-main()
-{
-    int n,c=0,m=0;
-    double d;
-    cin>>n;
-    vector<int>v,w;
-    for(int i=0;i<n;i++)
+    #include<bits/stdc++.h>
+    using namespace std;
+    typedef double ll;
+    main()
     {
-        int o;cin>>o;
-        v.push_back(o);
-    }
-    for(int i=0;i<n;i++)
-    {
-        int o;cin>>o;
-        w.push_back(o);
-    }
-    vector<double>x;
-    map<double,int>mp;
-    for(int i=0;i<n;i++)
-    {
-        if(v[i]==0)
+        int n,c=0,m=0;
+        ll d;
+        cin>>n;
+        vector<int>v,w;
+        for(ll i=0;i<n;i++)
         {
-            if(w[i]==0)
+            ll o;cin>>o;
+            v.push_back(o);
+        }
+        for(ll i=0;i<n;i++)
+        {
+            ll o;cin>>o;
+            w.push_back(o);
+        }
+        vector<ll>x;
+        map<double,int>mp;
+        for(ll i=0;i<n;i++)
+        {
+            if(v[i]==0)
             {
-                c++;
+               if(w[i]==0)
+               {
+                   c++;
+               }
             }
+            else
+            {
+                d=-1*(((double)(5*w[i]))/((double)(v[i])));
+                x.push_back(d);
+                mp[d]++;
+            }
+
         }
-        else
+        if(x.size()==0)
         {
-            d=-1*(((double)(5*w[i]))/((double)(v[i])));
-            x.push_back(d);
-            mp[d]++;
+            cout<<c;return 0;
         }
+        /*
+        sort(x.begin(),x.end());
+        for(int i=0;i<x.size();i++)
+            cout<<x[i]<<" ";
+        cout<<endl;
+        */
+        map<double,int>::iterator it;
+        vector<int>f;
+        for(it=mp.begin();it!=mp.end();it++)
+        {
+            //cout<<(*it).first<<" "<<(*it).second<<endl;
+            f.push_back((*it).second);
+        }
+        int bal=*max_element(f.begin(),f.end());
+        cout<<bal+c<<endl;
+        return 0;
 
     }
-    if(x.size()==0)
-    {
-        cout<<c;return 0;
-    }
-    map<double,int>::iterator it;
-    vector<int>f;
-    for(it=mp.begin();it!=mp.end();it++)
-    {
-        //cout<<(*it).first<<" "<<(*it).second<<endl;
-        f.push_back((*it).second);
-    }
-    int bal=*max_element(f.begin(),f.end());
-    cout<<bal+c<<endl;
-    return 0;
-}
