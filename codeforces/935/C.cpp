470e059f50f9b479eb26ee2af4c036316e7d57b6
autul2017831021 <alexiautul@gmail.com>
2020-03-22 23:55:00 +0600

    Add solution for problem `C - Fifa and Fafa` Link: https://codeforces.com/contest/935/problem/C Tags: geometry, *1600 Ref: https://codeforces.com/contest/935/submission/73984949

--- codeforces/935/C.cpp
+++ codeforces/935/C.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define eps 1e-7
 double distance(double x1, double y1, double x2, double y2)
 {
-    return sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
+    return sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
 }
 main()
 {
@@ -19,43 +19,21 @@ main()
     {
         double ex=R-dist;
         r=2*R-ex;
-        r=r/2.0;
-        double rr=r-dist;
-        ///cout<<r<<endl<<rr<<endl;
-        for(double i=0.0;i<=360;i+=0.0001)
-        {
-            double cd,cx,cy,d;
-            cd=(i*pi)/180.0;
-            cx=(rr*cos(cd))+x1;
-            cy=(rr*sin(cd))+y1;
-            d=distance(x2,y2,cx,cy);
-            double dd=abs(d-r);
-            ///cout<<i<<endl<<cd<<endl<<cx<<endl<<cy<<endl<<d+r<<endl<<d<<endl<<"bal"<<endl;
-            if(dd<=eps)
-            {
-                printf("%.15f %.15f %.15f",cx,cy,d);
-                break;
-            }
-        }
-    }
-}
-/*
-alternate AC hobe sure..
-        double ex=R-dist;
-        r=2*R-ex;
-        for(double i=0.0;i<=360;i+=0.0001)
+
+        for(double i=0.0;i<=360.0;i+=0.0001)
         {
             double cd,cx,cy,d;
             cd=(i*pi)/180.0;
             cx=(R*cos(cd))+x1;
             cy=(R*sin(cd))+y1;
             d=distance(x2,y2,cx,cy);
-            double dd=abs(d-r);
-            ///cout<<i<<endl<<cd<<endl<<cx<<endl<<cy<<endl<<d+r<<endl<<d<<endl<<"bal"<<endl;
-            if(dd<=eps)
+            d=abs(d-r);
+            if(d<=eps)
             {
-                printf("%.15f %.15f %.15f",(cx+x2)/2.0,(cy+y2)/2.0,d/2.0);
+                printf("%.15f %.15f %.15f",(cx+x2)/2.0,(cy+y2)/2.0,r/2.0);
                 break;
             }
         }
-*/
+    }
+
+}
