161b1ba4c5e7d7c095b6415d817821221bbd7f55
autul2017831021 <alexiautul@gmail.com>
2020-03-26 03:01:00 +0600

    Add solution for problem `B - Interesting drink` Link: https://codeforces.com/contest/706/problem/B Tags: binary search, dp, implementation, *1100 Ref: https://codeforces.com/contest/706/submission/74336348

--- codeforces/706/B.cpp
+++ codeforces/706/B.cpp
@@ -6,11 +6,11 @@ main()
     ll n,q;
     cin>>n;
     ll a[n+10];
-    for(ll i=1;i<n+1;i++)
+    for(ll i=0;i<n;i++)
     {
         cin>>a[i];
     }
-    sort(a+1,a+n+1);
+    sort(a,a+n);
     cin>>q;
     ll p[q+10];
     for(int i=1;i<=q;i++)
@@ -20,7 +20,7 @@ main()
     }
     for(ll i=1;i<=q;i++)
     {
-        cout<<upper_bound(a+1,a+n+1,p[i])-(a+1)<<endl;
+        cout<<upper_bound(a,a+n,p[i])-a<<endl;
     }
 }
 
