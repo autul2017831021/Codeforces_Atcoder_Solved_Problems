31e2a0f5ca3a9f19197276dd8165a32c03091a5a
autul2017831021 <alexiautul@gmail.com>
2020-04-09 23:19:00 +0600

    Add solution for problem `D - Petya and His Friends` Link: https://codeforces.com/contest/66/problem/D Tags: constructive algorithms, math, number theory, *1700 Ref: https://codeforces.com/contest/66/submission/76020245

--- codeforces/66/D.cpp
+++ codeforces/66/D.cpp
@@ -6,16 +6,16 @@ main()
     long long m,k;cin>>m;
     if(m>3)
     {
-        cout<<"143"<<endl<<"91"<<endl<<"77"<<endl;
+        cout<<"10"<<endl<<"15"<<endl<<"6"<<endl;
         for(int i=2;i<=m-2;i++)
         {
-            long long ans=77*i;
+            long long ans=10*i;
             cout<<ans<<endl;
         }
     }
     else if(m==3)
     {
-        cout<<"143"<<endl<<"91"<<endl<<"77"<<endl;
+        cout<<"10"<<endl<<"15"<<endl<<"6"<<endl;
     }
     else if(m==2)
         cout<<"-1";
