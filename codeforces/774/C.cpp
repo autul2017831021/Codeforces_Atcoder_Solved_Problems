15e22e5f38751c86aa136ef802efd75a622b2ccd
autul2017831021 <alexiautul@gmail.com>
2020-04-20 14:17:00 +0600

    Add solution for problem `C - Maximum Number` Link: https://codeforces.com/contest/774/problem/C Tags: *special problem, constructive algorithms, greedy, implementation, *1200 Ref: https://codeforces.com/contest/774/submission/77339621

--- codeforces/774/C.cpp
+++ codeforces/774/C.cpp
@@ -1,39 +1,39 @@
-    using namespace std;
-    #include<bits/stdc++.h>
-    typedef long long int ll;
-    typedef double dl;
-    #define pb push_back
-    #define bg begin()
-    #define en end()
-    #define rbg rbegin()
-    #define ren rend()
-    #define sz size()
-    #define r0 return 0
-    #define F first
-    #define S second
-    main()
+using namespace std;
+#include<bits/stdc++.h>
+typedef long long int ll;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0
+#define F first
+#define S second
+main()
+{
+    ll a,b;
+    cin>>a;
+    if(a%2)
     {
-        ll a,b;
-        cin>>a;
-        if(a%2)
+        a=a-3;
+        cout<<"7";
+        while(1)
         {
-            a=a-3;
-            cout<<"7";
-            while(1)
-            {
-                if(a<=0)
-                    break;
-                a=a-2;
-                cout<<"1";
-            }
-        }
-        else
-        {
-            while(1){
-                cout<<"1";
-                a=a-2;
-                if(a==0)
-                    break;
-              }
+            if(a<=0)
+                break;
+            a=a-2;
+            cout<<"1";
         }
     }
+    else
+    {
+        while(1){
+            cout<<"1";
+            a=a-2;
+            if(a==0)
+                break;
+          }
+    }
+}
