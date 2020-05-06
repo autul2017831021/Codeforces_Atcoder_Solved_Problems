46c103146703f14bb6ee14fde59f7f4f3f8728f9
autul2017831021 <alexiautul@gmail.com>
2020-05-06 21:12:00 +0600

    Add solution for problem `A - Puzzle Pieces` Link: https://codeforces.com/contest/1345/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1345/submission/79173276

---
+++ codeforces/1345/A.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 999999999
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES ");
+#define no printf("NO ");
+#define mone printf("-1 ");
+#define ne cin>>
+#define hoynai cout<<"hoynai"<<endl;
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int n,m;
+       ne n;ne m;
+       if(n==1 || m==1 || (n==2 && m==2))
+       {
+           yes;nl;
+       }
+       else
+       {
+           no;nl;
+       }
+   }
+}
