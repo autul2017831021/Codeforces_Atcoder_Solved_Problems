542378a431a3067896c1cf099a59d23a5084c50f
autul2017831021 <alexiautul@gmail.com>
2020-05-18 23:46:00 +0600

    Add solution for problem `A - Sequence with Digits` Link: https://codeforces.com/contest/1355/problem/A Tags: brute force, implementation, math, *1200 Ref: https://codeforces.com/contest/1355/submission/80628406

---
+++ codeforces/1355/A.cpp
@@ -0,0 +1,106 @@
+/*  ---------------------
+    |      EIKHANE KI   |
+    |        MAAGI      |
+    ---------------------
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES \n");
+#define no printf("NO \n");
+#define mone printf("-1 ");
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+void kor()
+{
+
+}
+ll mi(ll an)
+{
+    ll x=inf;
+    while(an)
+    {
+        x=min(x,an%10);an=an/10;
+    }
+    return x;
+}
+ll ma(ll an)
+{
+    ll x=ninf;
+    while(an)
+    {
+        x=max(x,an%10);an=an/10;
+    }
+    return x;
+}
+main()
+{
+    ll t;ne t;
+    while(t--)
+    {
+        ll n,k;ne n;ne k;ll an=n;
+        for(int i=1;i<k;i++)
+        {
+            ll x=mi(an);
+            ll y=ma(an);
+            if(x==0 || y==0)
+                break;
+            an=an+x*y;
+        }
+        de an;nl;
+    }
+}
