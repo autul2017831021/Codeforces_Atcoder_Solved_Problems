d85259b9e200c59b8df0542a5c9805cbab778cdf
autul2017831021 <alexiautul@gmail.com>
2020-05-31 20:55:00 +0600

    Add solution for problem `A - Odd Selection` Link: https://codeforces.com/contest/1363/problem/A Tags: brute force, implementation, math, *1200 Ref: https://codeforces.com/contest/1363/submission/82068483

---
+++ codeforces/1363/A.cpp
@@ -0,0 +1,111 @@
+/*  ---------------------
+    |      EIKHANE KI   |
+    |        MAAGI      |
+    ---------------------
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n");
+#define no printf("NO\n");
+#define mone printf("-1 ");
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+void kor()
+{
+
+}
+main()
+{
+    int t;ne t;
+    while(t--)
+    {
+        ll n,x;ne n;ne x;
+        ll o=0,e=0;vc v;
+        for(int i=0;i<n;i++)
+        {
+            ll c;ne c;
+            if(c%2){
+                o++;
+                v.pb(1);
+            }
+            else{
+                e++;
+                v.pb(0);
+            }
+        }sort(v.rbg,v.ren);
+        if(o>0)
+        {
+            bool f=0;
+            for(ll i=1;i<=x;i=i+2){
+                if(i<=o && (x-i)<=e)
+                {
+                    f=1;break;
+                }
+            }
+            if(f){
+                de "YES";nl;
+            }
+            else{
+                de "NO";nl;
+            }
+        }
+        else{
+            de "NO";nl;
+        }
+    }
+}
