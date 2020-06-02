2b9d65a3b0ddf1cc746ce86df7a31ed4cf89a7af
autul2017831021 <alexiautul@gmail.com>
2020-06-02 20:58:00 +0600

    Add solution for problem `B - Subsequence Hate` Link: https://codeforces.com/contest/1363/problem/B Tags: implementation, strings, *1400 Ref: https://codeforces.com/contest/1363/submission/82320187

---
+++ codeforces/1363/B.cpp
@@ -0,0 +1,103 @@
+/*  ---------------------
+    |      EIKHANE KI   |
+    |        MAAGI      |
+    ---------------------
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n");
+#define no printf("NO\n");
+#define mone printf("-1 ");
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+void kor(string s)
+{
+    ll ans=inf;
+    ll a=0,b=0;
+    for(int i=0;i<s.sz;i++)
+    {
+        if(s[i]=='0')
+            a++;
+        else
+            b++;
+    }
+    ll x=0,y=0;
+    for(int i=0;i<s.sz;i++)
+    {
+        if(s[i]=='0')
+            x++;
+        else
+            y++;
+        ans=min(ans,x+b-y);
+        ans=min(ans,y+a-x);
+    }
+    de ans;nl;
+}
+main()
+{
+    int t;ne t;
+    while(t--)
+    {
+        string s;
+        ne s;
+        if(s.sz>2){
+            kor(s);
+        }
+        else{de "0";nl;}
+    }
+}
