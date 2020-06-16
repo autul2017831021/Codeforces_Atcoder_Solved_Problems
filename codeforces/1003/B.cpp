2e464a9ca0cceb04f64056e684fe3279c4600b1d
autul2017831021 <alexiautul@gmail.com>
2020-06-17 00:29:00 +0600

    Add solution for problem `B - Binary String Constructing` Link: https://codeforces.com/contest/1003/problem/B Tags: constructive algorithms, *1300 Ref: https://codeforces.com/contest/1003/submission/84031686

---
+++ codeforces/1003/B.cpp
@@ -0,0 +1,137 @@
+/*  ---------------------
+    |      EIKHANE KI   |
+    |        MAAGI      |
+    ---------------------
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n");
+#define no printf("NO\n");
+#define mone printf("-1 ")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+void kor(ll a,ll b,ll x){
+    ///a=0,b=1;
+    string s="";
+    for(int i=0;i<x;i++){
+        if(i%2){
+            s+="1";
+            b--;
+        }
+        else{
+            s+="0";
+            a--;
+        }
+    }
+    if(s[s.sz-1]=='0')
+    {
+        for(int i=0;i<a;i++)
+            s+="0";
+        for(int i=0;i<b;i++)
+            s+="1";
+    }
+    else
+    {
+        for(int i=0;i<b;i++)
+            s+="1";
+        for(int i=0;i<a;i++)
+            s+="0";
+    }
+    de s;
+}
+void por(ll a,ll b,ll x)
+{
+    ///a=1,b=0;
+    string s="";
+    for(int i=0;i<x;i++){
+        if(i%2){
+            s+="0";
+            b--;
+        }
+        else{
+            s+="1";
+            a--;
+        }
+    }
+    if(s[s.sz-1]=='0')
+    {
+        for(int i=0;i<b;i++)
+            s+="0";
+        for(int i=0;i<a;i++)
+            s+="1";
+    }
+    else
+    {
+        for(int i=0;i<a;i++)
+            s+="1";
+        for(int i=0;i<b;i++)
+            s+="0";
+    }
+    de s;
+}
+main()
+{
+   ll a,b,x;
+   ne a;ne b;ne x;
+   if(a>=b){
+       kor(a,b,x);
+   }
+   else{
+       por(b,a,x);
+   }
+}
