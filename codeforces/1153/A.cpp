874a5fc1f9bdb743f97a456b863db017006848ea
autul2017831021 <alexiautul@gmail.com>
2020-06-20 17:50:00 +0600

    Add solution for problem `A - Serval and Bus` Link: https://codeforces.com/contest/1153/problem/A Tags: brute force, math, *1000 Ref: https://codeforces.com/contest/1153/submission/84410578

---
+++ codeforces/1153/A.cpp
@@ -0,0 +1,99 @@
+/*    YOU VIRGIN ASSHOLE
+      WHY ARE YOU HERE?    */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n");
+#define no printf("NO\n");
+#define mone printf("-1 ")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+main()
+{
+    ll n,t,a,b;
+    ne n;ne t;
+    vp v;
+    for(ll i=0;i<n;i++){
+        ne a;ne b;
+        if(a>=t){
+            v.pb({a,i+1});
+        }
+        else{
+            while(1){
+                a=a+b;
+                if(a>=t){
+                    v.pb({a,i+1});
+                    break;
+                }
+            }
+        }
+    }
+    sort(v.bg,v.en);
+    de v[0].S;
+}
