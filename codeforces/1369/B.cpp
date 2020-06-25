c01747fc9ad81760c923f46ff3ab39393dee1d71
autul2017831021 <alexiautul@gmail.com>
2020-06-26 04:03:00 +0600

    Add solution for problem `B - AccurateLee` Link: https://codeforces.com/contest/1369/problem/B Tags: greedy, implementation, strings, *1200 Ref: https://codeforces.com/contest/1369/submission/85076512

---
+++ codeforces/1369/B.cpp
@@ -0,0 +1,104 @@
+/*    YOU VIRGIN ASSHOLE
+      WHY ARE YOU HERE?    */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+main()
+{
+    int t;ne t;
+    while(t--)
+    {
+        int l;ne l;
+        string s;ne s;
+        ll a=-1,b=-1;
+        for(int i=s.sz-1;i>=0;i--){
+            if(s[i]=='0'){a=i;break;}
+        }
+        for(int i=0;i<s.sz;i++){
+            if(s[i]=='1'){
+                b=i;break;
+            }
+        }
+        if(b>a || a==-1 || b==-1){
+            de s;nl;continue;
+        }
+        for(int i=0;i<b;i++){
+            de s[i];
+        }
+        for(int i=a;i<s.sz;i++){
+            de s[i];
+        }nl;
+    }
+}
