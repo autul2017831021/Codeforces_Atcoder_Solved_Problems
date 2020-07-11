f7efcfa1604988edc640ad929b00421ce57bcb85
autul2017831021 <alexiautul@gmail.com>
2020-07-11 22:52:00 +0600

    Add solution for problem `C - Omkar and Baseball` Link: https://codeforces.com/contest/1372/problem/C Tags: constructive algorithms, math, *1500 Ref: https://codeforces.com/contest/1372/submission/86586860

---
+++ codeforces/1372/C.cpp
@@ -0,0 +1,105 @@
+/*    YOU VIRGIN ASSHOLE
+      WHY ARE YOU HERE?    */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+/// priority_queue<ll,vector<ll>,greater<ll> >
+bool pusu(pair<char,int>a,pair<char,int>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+
+int main()
+{
+    int t;ne t;
+    while(t--)
+    {
+        ll n;ne n;
+        ll ans=0;
+        vc v;ml m;ml ::iterator it;
+        for(int i=1;i<=n;i++){
+            ll x;ne x;
+            if(x!=i){
+                v.pb(i);
+                m[x]=i;
+            }
+        }
+        ll s=v.sz;
+        if(v.sz==0){de ans;nl;continue;}
+        if(v.sz==n){de "1";nl;continue;}
+        ll d=v[s-1]-v[0]+1;
+        if(d==s)
+            ans++;
+        else
+            ans=ans+2;
+        de ans;nl;
+    }
+}
