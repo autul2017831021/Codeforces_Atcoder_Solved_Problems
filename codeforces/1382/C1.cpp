3e93bcd4f4cfea92454dcd3431e010e795d622a5
autul2017831021 <alexiautul@gmail.com>
2020-07-21 23:46:00 +0600

    Add solution for problem `C1 - Prefix Flip (Easy Version)` Link: https://codeforces.com/contest/1382/problem/C1 Tags: constructive algorithms, data structures, strings, *1300 Ref: https://codeforces.com/contest/1382/submission/87599953

---
+++ codeforces/1382/C1.cpp
@@ -0,0 +1,109 @@
+/*    YOU VIRGIN ASSHOLE
+      WHY ARE YOU HERE?    */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 10000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+/// priority_queue<ll,vector<ll>,greater<ll> >
+bool pusu(pair<char,int>a,pair<char,int>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+int main()
+{
+    int tt;ne tt;
+    while(tt--)
+    {
+        int n;ne n;
+        string s,t;
+        ne s;
+        ne t;
+        vc v;
+        for(int i=1;i<n;i++){
+            if(s[i]!=s[i-1]){
+                v.pb(i);
+                if(s[0]=='0')s[0]='1';
+                else{s[0]='0';}
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            if(s[0]!=t[i]){
+                v.pb(i+1);
+                if(s[0]=='0')s[0]='1';
+                else{s[0]='0';}
+            }
+        }
+        de v.sz;sp;
+        for(int i=0;i<v.sz;i++){
+            de v[i];sp;
+        }nl;
+    }
+}
+
