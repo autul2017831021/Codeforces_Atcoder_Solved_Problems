3059dbb9e6962f1e2564fe4bbd4f3138434bbc6b
autul2017831021 <alexiautul@gmail.com>
2020-07-30 21:22:00 +0600

    Add solution for problem `A - Captain Flint and Crew Recruitment` Link: https://codeforces.com/contest/1388/problem/A Tags: brute force, greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1388/submission/88492298

---
+++ codeforces/1388/A.cpp
@@ -0,0 +1,107 @@
+/*    YOU VIRGIN ASSHOLE
+      WHY ARE YOU HERE?    */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 10000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+/// priority_queue<ll,vector<ll>,greater<ll> >
+bool pusu(pair<char,int>a,pair<char,int>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int n;cin>>n;
+        if(n>30){
+            cout<<"YES"<<endl;
+            if(n!=36 && n!=40 && n!=44){
+                de "10 14 6 ";de n-(10+14+6);
+            }
+            else if(n==36){
+                de "5 6 10 15";
+            }
+            else if(n==44){
+                de "6 7 10 21";
+            }
+            else if(n==40){
+                de "22 10 6 2";
+            }
+            nl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
+
