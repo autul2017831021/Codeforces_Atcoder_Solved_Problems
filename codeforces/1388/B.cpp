5f5bbe96d989a95faae7afbb26ee2f4542ef57a9
autul2017831021 <alexiautul@gmail.com>
2020-07-30 21:23:00 +0600

    Add solution for problem `B - Captain Flint and a Long Voyage` Link: https://codeforces.com/contest/1388/problem/B Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/1388/submission/88492473

---
+++ codeforces/1388/B.cpp
@@ -0,0 +1,105 @@
+/*    YOU VIRGIN ASSHOLE
+      WHY ARE YOU HERE?    */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 10000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+/// priority_queue<ll,vector<ll>,greater<ll> >
+bool pusu(pair<char,int>a,pair<char,int>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+int main()
+{
+    int test;
+    cin>>test;
+    for(int ii=0;ii<test;ii++)
+    {
+        ll n;cin>>n;
+        string s;
+        if(n<5)
+        {
+            s.pb('8');
+            n--;
+            for(int i=0;i<n;i++){
+                s.pb('9');
+            }
+            reverse(s.bg,s.en);
+        }
+        else{
+            ll d=n/4;
+            if(n%4)d++;
+            for(int i=0;i<n-d;i++){s.pb('9');}
+            for(int i=0;i<d;i++){s.pb('8');}
+        }
+        de s;nl;
+    }
+}
+
