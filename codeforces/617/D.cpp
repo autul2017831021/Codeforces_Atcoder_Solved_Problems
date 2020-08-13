7852e91beb255e4ba8c2c5339a82b5cd56638069
autul2017831021 <alexiautul@gmail.com>
2020-08-13 16:13:00 +0600

    Add solution for problem `D - Polyline` Link: https://codeforces.com/contest/617/problem/D Tags: constructive algorithms, implementation, *1700 Ref: https://codeforces.com/contest/617/submission/89783333

---
+++ codeforces/617/D.cpp
@@ -0,0 +1,91 @@
+/*        ASSHOLE
+      WHY ARE YOU HERE?    */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 10000000000000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+int main()
+{
+    ll a,b,c,x,y,z,r=1;
+    cin>>a>>x>>b>>y>>c>>z;
+    if((a==b && b==c) || (x==y && y==z))r=1;
+    else if(a==b && (max(x,y)<=z || min(x,y)>=z) )r++;
+    else if(a==c && (max(x,z)<=y || min(x,z)>=y) )r++;
+    else if(b==c && (max(z,y)<=x || min(z,y)>=x) )r++;
+    else if(x==y && (max(a,b)<=c || min(a,b)>=c) )r++;
+    else if(y==z && (max(b,c)<=a || min(b,c)>=a) )r++;
+    else if(z==x && (max(a,c)<=b || min(a,c)>=b) )r++;
+    else r=3;
+    de r;
+}
