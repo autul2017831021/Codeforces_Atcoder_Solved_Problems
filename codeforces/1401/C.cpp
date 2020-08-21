d2f384f3f9b238ac3f724dab89051111b8c09532
autul2017831021 <alexiautul@gmail.com>
2020-08-21 21:52:00 +0600

    Add solution for problem `C - Mere Array` Link: https://codeforces.com/contest/1401/problem/C Tags: constructive algorithms, math, number theory, sortings, *1300 Ref: https://codeforces.com/contest/1401/submission/90597478

---
+++ codeforces/1401/C.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+int main()
+{
+    int t;ne t;
+    while(t--)
+    {
+        ll n;ne n;
+        vc v,w,a;
+        for(int i=0;i<n;i++){
+            ll x;ne x;
+            v.pb(x);w.pb(x);
+        }
+        sort(w.bg,w.en);
+        for(int i=0;i<n;i++){
+            if(w[i]!=v[i]){a.pb(v[i]);}
+        }
+        bool f=1;
+        ll bal=*min_element(v.bg,v.en);
+        for(int i=0;i<a.sz;i++){
+            if(a[i]%bal!=0){f=0;break;}
+        }
+        if(f)yes;
+        else no;
+    }
+}
