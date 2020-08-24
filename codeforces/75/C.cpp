36ff0aa78932dd1e416efb2a5b34f3cec4facaef
autul2017831021 <alexiautul@gmail.com>
2020-08-24 20:45:00 +0600

    Add solution for problem `C - Modified GCD` Link: https://codeforces.com/contest/75/problem/C Tags: binary search, number theory, *1600 Ref: https://codeforces.com/contest/75/submission/90835791

---
+++ codeforces/75/C.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000009
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+bool pusu(pair<ll,ll>a,pair<ll,ll>b)
+{
+    return b.second>a.second;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+int Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+vector<int>v;
+void printDivisors(int n)
+{
+    for(long long i=1;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+        {
+            if(n/i==i){
+                v.push_back(i);
+            }
+            else {
+                v.push_back(i);
+                v.push_back(n/i);
+            }
+        }
+    }
+    sort(v.begin(),v.end());
+}
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    int tt;ne tt;
+    int g=__gcd(x,y);
+    printDivisors(g);
+    while(tt--)
+    {
+        int a,b,ans=-1;
+        cin>>a>>b;
+        for(int i=0;i<v.sz;i++){
+            if(v[i]>=a && v[i]<=b)ans=max(ans,v[i]);
+        }
+        de ans;nl;
+    }
+}
+
