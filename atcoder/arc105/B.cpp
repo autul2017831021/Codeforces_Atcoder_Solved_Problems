0ee250ef5612fa3a5f57c3f31f033559645f7159
autul2017831021 <alexiautul@gmail.com>
2020-10-11 20:01:00 +0600

    Add solution for problem `B - MAX-=min` Link: https://atcoder.jp/contests/arc105/tasks/arc105_b Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/arc105/submissions/17344640

---
+++ atcoder/arc105/B.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 1000000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define modd 1000000007
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+/// priority_queue<ll,vector<ll>,greater<ll> >
+bool pusu(pair<pair<ll,ll>,ll>a,pair<pair<ll,ll>,ll>b)
+{
+    if(a.F.S>b.F.S || (a.F.S==b.F.S && a.F.F<b.F.F))return true;
+    return false;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y,ll mod)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+int main()
+{
+    int t;t=1;
+    while(t--)
+    {
+        ll n;ne n;
+        vc v;
+        ml m;
+        ll g=0;
+        for(int i=0;i<n;i++){
+            ll c;ne c;
+            g=__gcd(c,g);
+            m[c]++;
+            if(m[c]==1){
+                v.pb(c);
+            }
+        }
+        de g;
+        /*
+        sort(v.bg,v.en);
+        priority_queue<ll>a;
+        ll x=v[0];
+        for(int i=1;i<v.sz;i++){
+            a.push(v[i]);
+        }
+        while(!a.empty())
+        {
+            ll bal=a.top();
+            a.pop();
+            ll sal=bal-x;
+            //de sal;sp;de x;nl;
+            if(bal==x)break;
+            if(sal<x){a.push(x);x=sal;}
+            else{
+                if(m[sal]==0)a.push(sal);
+            }
+        }
+        de x;
+        */
+    }
+}
