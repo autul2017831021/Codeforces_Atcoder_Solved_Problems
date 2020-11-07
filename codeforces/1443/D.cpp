57363b1f313bea82d8e2b42f99f177a04f42ab20
autul2017831021 <alexiautul@gmail.com>
2020-11-07 22:53:00 +0600

    Add solution for problem `D - Extreme Subtraction` Link: https://codeforces.com/contest/1443/problem/D Tags: dp, greedy, math, *1800 Ref: https://codeforces.com/contest/1443/submission/97860656

---
+++ codeforces/1443/D.cpp
@@ -0,0 +1,153 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 10000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define pi acos(-1.0)
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+/// priority_queue<ll,vector<ll>,greater<ll> >
+bool pusu(pair<int,int>a,pair<int,int>b)
+{
+    if(a.F<b.F || (a.F==b.F && a.S>b.S))return true;
+    return false;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/(__gcd(a,b));
+}
+void yonko()
+{
+    int n;ne n;
+    int a[n+10];
+    int b[n+10];
+    for(int i=0;i<n;i++){
+        ne a[i];
+        b[n-1-i]=a[i];
+    }
+    int ma=*max_element(a,a+n);
+    int add=a[0]+a[n-1];
+    if(add<ma){
+        no;return;
+    }
+    bool f=1,g=1;
+    int pivot=a[0],minus=0;
+    for(int i=1;i<n-1;i++){
+        if(a[i]>pivot){
+            if(minus==0)minus=pivot;
+            pivot=a[i]-pivot;
+            for(int j=i+1;j<n;j++){
+                int c=a[j]-minus;
+                if(c>=pivot){
+                    pivot=c;
+                }
+                else{
+                    c=a[j]-pivot;
+                    if(c<0){f=0;break;}
+                    minus=c;
+                }
+            }
+            break;
+        }
+        else{
+            pivot=a[i];
+        }
+    }
+    for(int i=0;i<n;i++){
+        a[i]=b[i];
+    }
+    pivot=a[0];
+    minus=0;
+    for(int i=1;i<n-1;i++){
+        if(a[i]>pivot){
+            if(minus==0)minus=pivot;
+            pivot=a[i]-pivot;
+            for(int j=i+1;j<n;j++){
+                int c=a[j]-minus;
+                if(c>=pivot){
+                    pivot=c;
+                }
+                else{
+                    c=a[j]-pivot;
+                    if(c<0){g=0;break;}
+                    minus=c;
+                }
+            }
+            break;
+        }
+        else{
+            pivot=a[i];
+        }
+    }
+    if(g || f){yes;}
+    else no;
+}
+int main()
+{
+    int t;ne t;
+    while(t--)
+    {
+        yonko();
+    }
+}
+
