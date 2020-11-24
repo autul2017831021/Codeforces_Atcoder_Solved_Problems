53a3dea08a1b36433313ee9e70d8cb9f6b0df61c
autul2017831021 <alexiautul@gmail.com>
2020-11-24 20:44:00 +0600

    Add solution for problem `B - Unique Bid Auction` Link: https://codeforces.com/contest/1454/problem/B Tags: implementation, *800 Ref: https://codeforces.com/contest/1454/submission/99420508

---
+++ codeforces/1454/B.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<pair<ll,ll> > vp;
+typedef vector<ll> vc;
+typedef map<ll,ll> ml;
+typedef ostringstream OS ;
+typedef stringstream SS ;
+typedef double dl;
+#define pb push_back
+#define bg begin()
+#define en end()
+#define rbg rbegin()
+#define ren rend()
+#define sz size()
+#define r0 return 0;
+#define F first
+#define S second
+#define inf 10000000000
+#define ninf -1000000000
+#define its (*it).S
+#define itf (*it).F
+#define nl printf("\n")
+#define sp printf(" ")
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define mone printf("-1\n")
+#define ne cin>>
+#define de cout<<
+#define hoynai cout<<"hoynai"<<endl;
+#define mod 1000000007
+#define pi acos(-1.0)
+#define FAST_FUCK ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+/// priority_queue<ll,vector<ll>,greater<ll> >
+bool pusu(pair<int,int>a,pair<int,int>b)
+{
+    if(a.F<b.F || (a.F==b.F && a.S>b.S))return true;
+    return false;
+}
+string ntos(ll n) {
+     OS str1 ;
+     str1 << n ;
+     return str1.str();
+}
+ll ston(string s) {
+     ll x ;
+     SS str1(s);
+     str1 >> x ;
+     return x ;
+}
+ll bigmod(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x)%mod;
+        y=y>>1;
+        x= (x*x)%mod;
+    }
+    return res;
+}
+ll Pow(ll x, ll y)
+{
+    ll res = 1;
+    while(y>0)
+    {
+        if(y&1)
+            res = (res*x);
+        y=y>>1;
+        x= (x*x);
+    }
+    return res;
+}
+ll lcm(ll a,ll b)
+{
+    return (a*b)/__gcd(a,b);
+}
+void A()
+{
+    int n;
+    ne n;
+    for(int i=1;i<n;i++){
+        de i+1;sp;
+    }
+    de 1;nl;
+}
+void B()
+{
+    int n;ne n;
+    ml m,p;
+    vc v;
+    set<ll>s;
+    ml ::iterator it;
+    for(int i=1;i<=n;i++){
+        int c;ne c;
+        v.pb(c);s.insert(c);m[c]=i;
+        p[c]++;
+    }
+    if(s.sz==1 && n>1){
+        mone;return;
+    }
+    for(it=p.bg;it!=p.en;it++){
+        if(its==1){
+            de m[itf];nl;return;
+        }
+    }
+    mone;return;
+}
+void C()
+{
+    
+}
+int main()
+{
+    int t;ne t;
+    while(t--)
+    {
+        //A();
+        B();
+        C();
+    }
+}
